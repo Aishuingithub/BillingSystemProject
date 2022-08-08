2dac9c620859351e96df4929de20164651be20a0
RansarjeA <RansarjeA@G09INXNP31953X1.g09.fujitsu.local>
2022-08-08 11:17:18 +0530

    First commit

---
+++ src/main/java/com/Product/BillingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingProjectApplication.class, args);
+	}
+
+}
